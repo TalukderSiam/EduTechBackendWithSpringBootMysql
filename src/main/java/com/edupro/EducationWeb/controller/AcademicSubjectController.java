56e0e4de2e5ef312dde0bb5df91967f151e993f7
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-03-18 00:48:34 +0600

    WIP: Changes in AcademicSubjectController before merge

--- src/main/java/com/edupro/EducationWeb/controller/AcademicSubjectController.java
+++ src/main/java/com/edupro/EducationWeb/controller/AcademicSubjectController.java
@@ -22,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/academicsubject")
-@CrossOrigin(origins = "http://localhost:5173")
+// @CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AcademicSubjectController {
     @Autowired
     private AcademicSubjectService academicSubjectService;
